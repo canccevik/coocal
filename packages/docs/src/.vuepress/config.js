deb68687ab3d694d3aeb7366aa4d6610cddd35f2
Can Çevik <mazlumcancevik@gmail.com>
2022-08-03 03:30:01 +0300

    create documentation project

---
+++ packages/docs/src/.vuepress/config.js
@@ -0,0 +1,42 @@
+const { description } = require('../../package')
+
+module.exports = {
+  title: 'Vuepress Docs Boilerplate',
+  description: description,
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/'
+      },
+      {
+        text: 'Config',
+        link: '/config/'
+      },
+      {
+        text: 'VuePress',
+        link: 'https://v1.vuepress.vuejs.org'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: ['', 'using-vue']
+        }
+      ]
+    }
+  },
+  plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom']
+}
